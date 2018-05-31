760f452be348cc70991ed6066a0b1c90bfaa33fb
Xavier59 <xavier.bruni@etudiant.univ-lille1.fr>
2018-05-31 21:16:43 +0200

    creation fenetre

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame.locals import *
+import src.ressources as ressources
+
+class Board:
+
+	def __init__(self):
+		pygame.init()
+		self.fenetre = pygame.display.set_mode((569, 600))
+		self.fond = pygame.image.load("img/teeko_board.jpg").convert()
+		self.plateau = []
+
+	def display(self):
+		self.fenetre.blit(self.fond, (0,0))
+		pygame.display.flip()
+
+class Joueur:
+
+	def __init__(self, is_real):
+		self.is_real = is_real
+
+	def jouer(self, plateau):
+		if is_real:
+			self.plateau = plateau
+		#else appeler predicat avec plateau
+
+def main():
+	board = Board()
+	board.display()
+
+if __name__ == '__main__':
+    main()
