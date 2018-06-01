dcd8d86bea2381b73f8d4c190ee05f093b15eb57
Xavier59 <xavier.bruni@etudiant.univ-lille1.fr>
2018-06-01 09:03:09 +0200

    debut recherche si mouvement possible

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ class Board:
 		pygame.init()
 		self.fenetre = pygame.display.set_mode((569, 600))
 		self.fond = pygame.image.load("../img/teeko_board.jpg").convert()
-		self.plateau = []
+		self.plateau = [0]*25
 
 	def display(self):
 		self.fenetre.blit(self.fond, (0,0))
@@ -16,11 +16,19 @@ class Board:
 	def checkWin(self, x, y):
 		pass
 
-	def isMovePossible(self, index):
-		pass
+	def isMovePossible(self, index, indexPion):
+		# verifier si tous les pions ont été posé
+		if self.plateau.count(0) < 8:
+			if self.plateau[i] == 0:
+				return True
+			return False
+		# sinon verifier que la distance soit egale a 1 ET que la case soit vide
+		else:
+			if self.plateau[i] == 0 and abs(indexPion-index) == 1 :
+				pass
 
 	def getCase(self, x, y):
-		if( (x > 90 and x < 150) or (x-150)%84 > 24):
+		if( (x > 90 and x < 150) or (x-150)%84 > 24 ) :
 			i = 0 if x < 150 else int(x / 84) - 1
 			print("dans la case d'index : "+str(i)+"\n")
 		else:
@@ -46,7 +54,7 @@ def main():
 			if event.type == QUIT:
 				exit()
 			if event.type == pygame.MOUSEBUTTONUP:
-				board.getCase(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]) 
+				board.getCase(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
 
 if __name__ == '__main__':
     main()
