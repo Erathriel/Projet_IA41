3ccc302fc603eda775c0415314008416dbebb47e
Xavier59 <xavier.bruni@etudiant.univ-lille1.fr>
2018-06-02 20:41:32 +0200

    jouer au teeko en 1v1 humain

--- src/main.py
+++ src/main.py
@@ -8,17 +8,34 @@ class Board:
 		self.fenetre = pygame.display.set_mode((569, 600))
 		self.fond = pygame.image.load("../img/teeko_board.jpg").convert()
 		self.jeton = [pygame.image.load("../img/jeton1.png").convert_alpha(), pygame.image.load("../img/jeton2.png").convert_alpha()]
-		self.plateau = [0]*25	
+		self.select = pygame.image.load("../img/select.png").convert()
+		self.plateau = [0]*25
 		#self.plateau = [1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 	def display(self):
+		self.displayBackground()
+		self.displayCases()
+		pygame.display.flip()
+
+	def displayBackground(self):
 		self.fenetre.blit(self.fond, (0,0))
+
+	def displayCases(self):
 		for i in range(0, 25):
 			if self.plateau[i] != 0:
 				self.fenetre.blit(self.jeton[self.plateau[i]-1], self.getCasePos(i))
+
+	def displayCase(self, index, _id, select):
+		self.displaySelect(select)
+		self.fenetre.blit(self.jeton[_id-1], self.getCasePos(index))
 		pygame.display.flip()
 
-	def displayCase(self, index, _id):
-		self.fenetre.blit(self.jeton[_id-1], self.getCasePos(index))
+	def displaySelect(self, index):
+		if(index == None):
+			return
+		self.displayBackground()
+		self.displayCases()
+		self.fenetre.blit(self.select, self.getCasePos(index))
+		self.fenetre.blit(self.jeton[self.plateau[index]-1], self.getCasePos(index))
 		pygame.display.flip()
 
 	def checkWin(self):
@@ -34,12 +51,15 @@ class Board:
 		if(index < 0 or self.plateau[index] != 0):
 			return False
 		# verifier si tous les pions ont été posé
-		if self.plateau.count(0) > 17:
+		elif self.plateau.count(0) > 17:
 			return True
+		# si tout les pions ont ete place alors un pion doit etre selectionne pour savoir lequel bouger
+		elif indexPion == None:
+			return False
 		# sinon verifier que la distance soit egale a 1 ET que la case soit vide
-		else:
-			if abs(indexPion-index) == 1 or (abs(indexPion - index) >= 4 and abs(indexPion-index) <= 6) :
+		elif abs(indexPion-index) == 1 or (abs(indexPion - index) >= 4 and abs(indexPion-index) <= 6) :
 				return True
+		else:
 			return False
 
 	def getCase(self, x, y):
@@ -49,7 +69,6 @@ class Board:
 			return i
 		else:
 			return -1
-		pass
 
 	def getCasePos(self, index):
 		x = 90 if index%5 == 0 else ((index%5)-1)*83+174
@@ -60,6 +79,8 @@ class Board:
 		if(self.isMovePossible(case, casePrev)):
 			if casePrev:
 				self.plateau[casePrev] = 0
+			else:
+				joueur.nb_pion_pose+=1
 			self.plateau[case] = joueur._id
 			self.display()
 			return True
@@ -83,6 +104,7 @@ def main():
 
 	joueurs = [Joueur(1, True), Joueur(2, True)]
 	swap = 1
+	select = None
 
 	joueurs[swap].play(board)
 	swap = abs(swap - 1)
@@ -93,19 +115,24 @@ def main():
 				exit()
 			if event.type == pygame.MOUSEBUTTONUP and joueurs[swap].is_real :
 				case = board.getCase(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-				if board.playMove(joueurs[swap], case, None):
+				if board.playMove(joueurs[swap], case, select):
 					jwin = board.checkWin()
 					if(jwin):
 						print("Le joueur "+str(jwin)+" a gagne")
 					joueurs[swap].play(board)
 					swap = abs(swap - 1)
+					select = None
+				elif case != -1 and joueurs[swap].nb_pion_pose == 4 and board.plateau[case] == joueurs[swap]._id:
+					select = case
+					board.displaySelect(case)
 			if event.type == pygame.MOUSEMOTION and joueurs[swap].is_real :
 				case = board.getCase(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-				if board.isMovePossible(case, None):
+				if board.isMovePossible(case, select):
 					board.display()
-					board.displayCase(case, joueurs[swap]._id)
+					board.displayCase(case, joueurs[swap]._id, select)
 				else:
 					board.display()
+					board.displaySelect(select)
 
 if __name__ == '__main__':
     main()
